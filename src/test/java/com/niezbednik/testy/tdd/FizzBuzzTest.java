a3b14eae7437cb651b5856a2529388df63a98256
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-11 01:28:34 +0200

    Add FizzBuzz kata and StringSum kata

---
+++ src/test/java/com/niezbednik/testy/tdd/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package com.niezbednik.testy.tdd;
+
+public class FizzBuzzTest {
+// Napisz program, który wypisuje numery od 1 do 100 i który:
+// - dla wielokrotności 3 wypisze "Fizz" zamiast numeru
+// - dla wielokrotności 5 wypisze "Buzz" zamiast numeru
+// - dla wielokrotności 3 i 5 wypisze "FizzBuzz"
+//
+// Podzielmy to na kroki, żeby łatwiej napisać kod i testy.
+// 1. Napisz test, który sprawdza wypisywanie liczb od 1 do 100. Zaimplementuj. Zrefaktoruj.
+// 2. Napisz test, który sprawdza, czy dla liczb podzielnych przez 3 wypisuje się "Fizz". Zaimplementuj. Zrefaktoruj.
+// 3. Napisz test, który sprawdza, czy dla liczb podzielnych przez 5 wypisuje się "Buzz". Zaimplementuj. Zrefaktoruj.
+// 4. Napisz test, który sprawdza, czy dla liczb podzielnych przez 3 i 5 wypisuje się "FizzBuzz". Zaimplementuj. Zrefaktoruj.
+
+    
+}
---
+++ src/test/java/com/niezbednik/testy/tdd/StringSumTest.java
@@ -0,0 +1,16 @@
+package com.niezbednik.testy.tdd;
+
+public class StringSumTest {
+// Napisz prostą klasę StringSum, która będzie miała jedną metodę o sygnaturze:
+// String sum(String number1, String number2)
+// która będzie akceptowała na wejściu jedynie liczby naturalne i będzie zwracała ich sumę. Jeśli liczba na wejściu
+// nie będzie liczbą naturalną należy zastąpić ją przez 0 (zero).
+//
+// 1. Zacznij z najprostszym testem dla pustego Stringa
+// 2. Stwórz prostą metodę o podanej sygnaturze
+// 3. Napisz test, który będzie testował działanie dla dwóch małych liczb
+// 4. Zaimplementuj kod aby test przechodził
+// 5. Jeśli test przeszedł - zrefaktoruj kod
+
+
+}
