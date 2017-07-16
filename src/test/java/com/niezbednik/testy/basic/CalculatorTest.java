636002bff3c6cba582fd7464fb79338923105243
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-16 22:41:38 +0200

    Add test for add adding two integers in Calculator

--- src/test/java/com/niezbednik/testy/basic/CalculatorTest.java
+++ src/test/java/com/niezbednik/testy/basic/CalculatorTest.java
@@ -1,5 +1,18 @@
 package com.niezbednik.testy.basic;
 
-public class CalculatorTest {
+import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void shouldAddIntegers() {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(1, 2);
+
+        assertThat(sum).isEqualTo(3);
+        // to samo można zapisać używając wbudowanych assercji TestNG jako:
+        // assertEquals(sum, 3);
+    }
 }
