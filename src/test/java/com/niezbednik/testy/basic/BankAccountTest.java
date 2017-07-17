a56a7f52c704ea4124c95560ec1456c0969647b8
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-17 23:37:17 +0200

    Add test for decreasing balance when withdrawing in BankAccount

--- src/test/java/com/niezbednik/testy/basic/BankAccountTest.java
+++ src/test/java/com/niezbednik/testy/basic/BankAccountTest.java
@@ -1,5 +1,20 @@
 package com.niezbednik.testy.basic;
 
+import org.assertj.core.data.Offset;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BankAccountTest {
 
+    @Test
+    public void shouldDecreaseBalanceWhenWithdrawing() {
+        BankAccount bankAccount = new BankAccount(10.0);
+
+        bankAccount.withdraw(1.88);
+
+        assertThat(bankAccount.getBalance()).isEqualTo(8.12, Offset.offset(0.1));
+        // to samo można zapisać używając wbudowanych assercji TestNG jako:
+        // assertEquals(bankAccount.getBalance(), 8.12, 0.1);
+    }
 }
