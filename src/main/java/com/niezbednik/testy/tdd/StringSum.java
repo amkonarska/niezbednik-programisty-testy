d7f4ba70dbeed715172e0e918dd1e7008e9d1ea2
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-18 08:21:06 +0200

    Refactor sum method in StringSum

--- src/main/java/com/niezbednik/testy/tdd/StringSum.java
+++ src/main/java/com/niezbednik/testy/tdd/StringSum.java
@@ -2,24 +2,19 @@ package com.niezbednik.testy.tdd;
 
 class StringSum {
     String sum(String argument1, String argument2) {
-        int numericArgument1;
+        int numericArgument1 = getIntValueOf(argument1);
+        int numericArgument2 = getIntValueOf(argument2);
 
-        try {
-            numericArgument1 = Integer.parseInt(argument1);
-        } catch (NumberFormatException exception) {
-            numericArgument1 = 0;
-        }
-
-        int numericArgument2;
-
-        try {
-            numericArgument2 = Integer.parseInt(argument2);
-        } catch (NumberFormatException exception) {
-            numericArgument2 = 0;
-        }
-        
         int sum = numericArgument1 + numericArgument2;
 
         return String.valueOf(sum);
+    }
+
+    private int getIntValueOf(String argument) {
+        try {
+            return Integer.parseInt(argument);
+        } catch (NumberFormatException exception) {
+            return 0;
+        }
     }
 }
