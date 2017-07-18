bbe3048826706ccba8571f8818da52c045fc418f
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-18 08:31:17 +0200

    Add not passing test for number nine in PrimeChecker to reproduce the error

--- src/test/java/com/niezbednik/testy/tdd/PrimeCheckerTest.java
+++ src/test/java/com/niezbednik/testy/tdd/PrimeCheckerTest.java
@@ -5,11 +5,18 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 
 public class PrimeCheckerTest {
-    // Nie działa dla 2 - dodajemy test, który odtwarza błąd
     @Test
     public void shouldReturnTrueForTwo() {
         PrimeChecker checker = new PrimeChecker();
 
         assertTrue(checker.isPrime(2));
+    }
+
+    // Nie działa dla 9 - dodajemy test, który odtwarza błąd
+    @Test
+    public void shouldReturnTrueForNine() {
+        PrimeChecker checker = new PrimeChecker();
+
+        assertTrue(checker.isPrime(9));
     }
 }
