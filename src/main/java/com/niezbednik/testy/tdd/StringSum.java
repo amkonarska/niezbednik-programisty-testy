9bba659e9804e202812ae50806a600937935ba67
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-18 08:00:37 +0200

    Add simplest StringSum implementation to make first test pass

---
+++ src/main/java/com/niezbednik/testy/tdd/StringSum.java
@@ -0,0 +1,8 @@
+package com.niezbednik.testy.tdd;
+
+class StringSum {
+    //Dodajemy pierwszą implementację metody - test przechodzi
+    String sum(String argument1, String argument2) {
+        return "0";
+    }
+}
