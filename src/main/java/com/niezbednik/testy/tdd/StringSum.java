7ff314ab9498b80eb1a1c0f6e822346af874b55d
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-18 08:18:43 +0200

    Add simplest implementation for summing non natural numbers for StringSum

--- src/main/java/com/niezbednik/testy/tdd/StringSum.java
+++ src/main/java/com/niezbednik/testy/tdd/StringSum.java
@@ -2,9 +2,22 @@ package com.niezbednik.testy.tdd;
 
 class StringSum {
     String sum(String argument1, String argument2) {
-        int numericArgument1 = Integer.parseInt(argument1);
-        int numericArgument2 = Integer.parseInt(argument2);
+        int numericArgument1;
 
+        try {
+            numericArgument1 = Integer.parseInt(argument1);
+        } catch (NumberFormatException exception) {
+            numericArgument1 = 0;
+        }
+
+        int numericArgument2;
+
+        try {
+            numericArgument2 = Integer.parseInt(argument2);
+        } catch (NumberFormatException exception) {
+            numericArgument2 = 0;
+        }
+        
         int sum = numericArgument1 + numericArgument2;
 
         return String.valueOf(sum);
