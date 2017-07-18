a6f6eb51d4ce6c306c99291023fca881b55e956d
Aleksandra Konarska <amkonarska@users.noreply.github.com>
2017-07-18 08:30:20 +0200

    Fix PrimeChecker to recognize two as a prime number

--- src/main/java/com/niezbednik/testy/tdd/PrimeChecker.java
+++ src/main/java/com/niezbednik/testy/tdd/PrimeChecker.java
@@ -1,21 +1,25 @@
 package com.niezbednik.testy.tdd;
 
 class PrimeChecker {
-	boolean isPrime(int n) {
-		if (n == 1) {
-			return false;
-		}
-		
-		if (n%2 == 0) {
-			return false;
-		}
-		
-		for(int i = 3; i * i < n; i+=2) {
-			if (n % i == 0) {
-				return false;
-			}
-		}
-		
-		return true;
-	}	
+    boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        if (n == 2) {
+            return true;
+        }
+
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i < n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
